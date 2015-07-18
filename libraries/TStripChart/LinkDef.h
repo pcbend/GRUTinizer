e25dedfd41aa8fc73de181d85962e3d9e5aa0367
pcbend <pcbend@gmail.com>
2015-07-18 15:28:56 -0400

    TStripChart (made with Eric) added.

---
+++ libraries/TStripChart/LinkDef.h
@@ -0,0 +1,11 @@
+// TStripChart.h TStripChartItem.h
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TStripChartItem+;
+#pragma link C++ class TStripChart+;
+
+#endif
