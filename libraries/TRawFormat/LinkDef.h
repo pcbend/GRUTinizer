703c09a9235a025f0f1cddff96f0f1313435b8e0
Peter Bender <pcbend@gmail.com>
2015-07-22 10:52:52 -0400

    Fixed bugs in reading files.
    Can now read events from either nscldaq or gretinadaq.

---
+++ libraries/TRawFormat/LinkDef.h
@@ -0,0 +1,24 @@
+// TRawFile.h TRawMultiFile.h TRawEvent.h
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+
+#pragma link C++ enum kFileType;
+
+#pragma link C++ class TRawEvent+;
+#pragma link C++ class TRawFile+;
+#pragma link C++ class TRawFileIn+;
+#pragma link C++ class TRawFileOut+;
+
+
+
+
+//#pragma link C++ class TRawMultiFile+;
+
+
+#endif
