a824a505fbb5b11313f90139425d357917f2f1f0
Peter Bender <pcbend@gmail.com>
2015-08-07 10:40:16 -0400

    initial commit of TChannel.

---
+++ libraries/TGRUTUtil/LinkDef.h
@@ -0,0 +1,18 @@
+//TChannel.h
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class TChannel-;
+
+
+
+
+
+#endif
+
+
+
