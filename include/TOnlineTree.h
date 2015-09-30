749167ee5530c51db625635e698043e479092ab2
Eric Lunderberg <lunderberg@nscl.msu.edu>
2015-09-30 13:24:19 -0400

    Actually added TOnlineTree (Where did it go?)

---
+++ include/TOnlineTree.h
@@ -0,0 +1,78 @@
+#ifndef _TONLINETREE_H_
+#define _TONLINETREE_H_
+
+#include "TCut.h"
+#include "TDirectory.h"
+#include "TTree.h"
+
+#include "TDetector.h"
+
+class TOnlineTree : public TTree {
+public:
+  TOnlineTree(const char* name = "circular", int circular_size = 32768);
+  virtual ~TOnlineTree();
+
+  void AddDetectorBranch(TDetector** det, const char* name);
+
+  void AddHistogram(const char* name,
+                    int bins, double low, double high, const char* varexp,
+                    const char* gate = "");
+
+  void AddHistogram(const char* name,
+                    int binsX, double lowX, double highX, const char* varexpX,
+                    int binsY, double lowY, double highY, const char* varexpY,
+                    const char* gate = "");
+
+  void RefillHistograms();
+
+  virtual Int_t Fill();
+
+  std::vector<std::string> GetStringLeaves();
+
+  void cd(){ saved_dir = gDirectory; directory.cd(); }
+  void popd() {
+    if(saved_dir) {
+      saved_dir->cd();
+    }
+    saved_dir = NULL;
+  }
+
+private:
+  void recurse_down(std::vector<std::string>& terminal_leaves, std::string current_branch, TBranch* branch);
+
+  TDirectory directory;
+  TDirectory* saved_dir;
+
+  long event_num;
+  long actual_event_num; // Because ROOT is obnoxious, see the src file
+  long last_fill;
+  int circular_size;
+
+  struct HistPattern1D {
+    std::string name;
+    TCut gate;
+    std::string varexp;
+    int bins;
+    double low, high;
+  };
+
+  struct HistPattern2D {
+    std::string name;
+    TCut gate;
+    std::string varexp;
+    std::string varexpX;
+    int binsX;
+    double lowX, highX;
+    std::string varexpY;
+    int binsY;
+    double lowY, highY;
+  };
+
+  std::vector<HistPattern1D> hist_patterns_1d;
+  std::vector<HistPattern2D> hist_patterns_2d;
+
+
+  ClassDef(TOnlineTree,0);
+};
+
+#endif /* _TONLINETREE_H_ */
---
+++ libraries/TGRUTint/TOnlineTree.cxx
@@ -0,0 +1,144 @@
+#include "TOnlineTree.h"
+
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TObject.h"
+#include "TRandom.h"
+#include "TRegexp.h"
+
+#include "TPreserveGDirectory.h"
+
+TOnlineTree::TOnlineTree(const char* name, int circular_size)
+  : TTree(name,name), directory(Form("%s_dir",name),Form("%s_dir",name)),
+    event_num(0), actual_event_num(0), last_fill(0),
+    circular_size(circular_size), saved_dir(NULL) {
+  SetDirectory(0);
+  SetCircular(circular_size);
+
+  Branch("event_num", &event_num, "event_num/L");
+}
+
+TOnlineTree::~TOnlineTree() { }
+
+void TOnlineTree::AddDetectorBranch(TDetector** det, const char* name){
+  Branch(name, name, det);
+}
+
+std::vector<std::string> TOnlineTree::GetStringLeaves() {
+  std::vector<std::string> output;
+
+  TIter leaves(TTree::GetListOfBranches());
+  TObject* obj = NULL;
+  while((obj = leaves.Next())){
+    recurse_down(output, obj->GetName(), (TBranch*)obj);
+  }
+
+  for(auto& name : output){
+    std::cout << name << std::endl;
+  }
+
+  return output;
+}
+
+void TOnlineTree::recurse_down(std::vector<std::string>& terminal_leaves, std::string current_branch, TBranch* branch){
+  if(branch->GetListOfBranches()->GetEntries() == 0){
+    terminal_leaves.push_back(current_branch);
+  } else {
+
+    TIter leaves(branch->GetListOfBranches());
+    TObject* obj = NULL;
+    while((obj = leaves.Next())){
+      recurse_down(terminal_leaves, current_branch + "." + obj->GetName(), (TBranch*)obj);
+    }
+  }
+}
+
+
+Int_t TOnlineTree::Fill(){
+  event_num = actual_event_num++;
+  Int_t output = TTree::Fill();
+
+  if(actual_event_num - last_fill > circular_size * 0.7){
+    RefillHistograms();
+  }
+
+  return output;
+}
+
+void TOnlineTree::AddHistogram(const char* name,
+                               int bins, double low, double high, const char* varexp,
+                               const char* gate){
+  TPreserveGDirectory preserve;
+  directory.cd();
+
+  HistPattern1D pat;
+
+  pat.name = Form("+%s",name);
+  pat.varexp = varexp;
+  pat.bins = bins;
+  pat.low = low;
+  pat.high = high;
+  pat.gate = gate;
+
+  // The histogram will be grabbed by the gDirectory, no memory leak
+  new TH1I(name,name,
+           bins, low, high);
+
+  hist_patterns_1d.push_back(pat);
+}
+
+void TOnlineTree::AddHistogram(const char* name,
+                               int binsX, double lowX, double highX, const char* varexpX,
+                               int binsY, double lowY, double highY, const char* varexpY,
+                               const char* gate){
+  TPreserveGDirectory preserve;
+  directory.cd();
+
+  HistPattern2D pat;
+
+  pat.name = Form("+%s",name);
+  pat.gate = gate;
+
+  pat.varexp = Form("%s:%s",varexpY,varexpX);
+
+  pat.varexpX = varexpX;
+  pat.binsX = binsX;
+  pat.lowX = lowX;
+  pat.highX = highX;
+
+  pat.varexpY = varexpY;
+  pat.binsY = binsY;
+  pat.lowY = lowY;
+  pat.highY = highY;
+
+  // The histogram will be grabbed by the gDirectory, no memory leak
+  new TH2I(name,name,
+           binsX, lowX, highX,
+           binsY, lowY, highY);
+
+  hist_patterns_2d.push_back(pat);
+}
+
+void TOnlineTree::RefillHistograms() {
+  TPreserveGDirectory preserve;
+  directory.cd();
+
+  for(auto& pattern : hist_patterns_1d) {
+    TTree::Project(pattern.name.c_str(), pattern.varexp.c_str(),
+                   pattern.gate, "", circular_size,
+                   GetEntries() - (actual_event_num - last_fill));
+  }
+
+  for(auto& pattern : hist_patterns_2d) {
+    TTree::Project(pattern.name.c_str(), pattern.varexp.c_str(),
+                   pattern.gate, "", circular_size,
+                   GetEntries() - (actual_event_num - last_fill));
+  }
+
+  last_fill = actual_event_num;
+}
