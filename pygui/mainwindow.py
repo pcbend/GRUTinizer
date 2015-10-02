e20b7d7c444a2912e1cdab345d3303481ca1e264
Eric Lunderberg <lunderberg@nscl.msu.edu>
2015-10-02 12:37:05 -0400

    Adding histogram-reading in buttons.py

--- pygui/mainwindow.py
+++ pygui/mainwindow.py
@@ -2,6 +2,7 @@
 
 import os
 import Tkinter as tk
+from tkFileDialog import askopenfilename
 import ttk
 
 import ROOT
@@ -18,6 +19,8 @@ class MainWindow(object):
         self.load_library('libGROOT.so')
 
         self.window = tk.Tk()
+        self._load_icons()
+
 
         self.refreshrate  = tk.IntVar()
         self.refreshrate.set(-1)
@@ -25,26 +28,49 @@ class MainWindow(object):
         self.plotlocation.set(1)
 
         self.canvases = []
+        self.files = []
 
         self._setup_GUI()
 
-    def load_library(self, library_name, grut_lib = True):
-        if grut_lib:
-            library_name = os.path.join(os.path.dirname(__file__),
-                                        '..','libraries',library_name)
-        ROOT.gSystem.Load(library_name)
+    def _load_icons(self):
+        self.icons = {}
+        self.icons['1d hist'] = tk.PhotoImage(
+            file = os.path.join(os.path.dirname(__file__),'resources','h1_t.ppm'))
+        self.icons['2d hist'] = tk.PhotoImage(
+            file = os.path.join(os.path.dirname(__file__),'resources','h2_t.ppm'))
 
-    def hello(self):
-        print "hello!"
+    def _setup_GUI(self):
+        self.window.geometry('350x700')
+        self.window.wm_title("hist-o-matic")
+        self._LoadMenuBar()
+        self.window.config(menu=self.menubar)
 
-    def open_canvas(self):
-        self.canvases.append(ROOT.GCanvas())
+        button = tk.Button(self.window,
+                           text='Load Remote File',fg="black",bg="goldenrod",
+                           command=self.LoadRemoteFile)
+        button.pack(fill=tk.X,expand=False)
 
-    def set_refresh(self):
-        print("refresh = " + str(self.refreshrate.get()))
+        button = tk.Button(self.window,
+                           text='Load Local File',fg="black",bg="goldenrod",
+                           command=self.LoadLocalFile)
+        button.pack(fill=tk.X,expand=False)
 
-    def set_plotlocation(self):
-        print("plot location = " + str(self.plotlocation.get()))
+        button = tk.Button(self.window,
+                           text='Jump into Interpreter',fg="black",bg="goldenrod",
+                           command=self.Interpreter)
+        button.pack(fill=tk.X,expand=False)
+
+        notebook = ttk.Notebook(self.window)
+
+        tree_page = ttk.Frame(notebook)
+        self._MakeTreeView(tree_page)
+        notebook.add(tree_page, text='Tree Viewer')
+
+        hist_page = ttk.Frame(notebook)
+        self._MakeHistView(hist_page)
+        notebook.add(hist_page, text='Histograms')
+
+        notebook.pack(fill=tk.BOTH,expand=True)
 
     def _LoadMenuBar(self):
         self.menubar  = tk.Menu(self.window)
@@ -55,7 +81,6 @@ class MainWindow(object):
         self._MakeOptStatMenu()
         self._MakePlotMenu()
         self._MakeHelpMenu()
-
 
     def _MakeFileMenu(self):
         filemenu = tk.Menu(self.menubar,tearoff=0)
@@ -109,11 +134,61 @@ class MainWindow(object):
         helpmenu = tk.Menu(self.menubar,tearoff=0)
         self.menubar.add_cascade(label="Send Help",menu=helpmenu)
 
+    def _MakeTreeView(self,parent):
+        self.tree = ttk.Treeview(parent)
+        self.tree.pack(fill=tk.BOTH,expand=True)
+        self._LoadTree()
+        self.tree.bind("<Double-1>", self.ParameterSelection)
+
+    def _MakeHistView(self,parent):
+        self.hists = ttk.Treeview(parent)
+        self.hists.pack(fill=tk.BOTH,expand=True)
+
     def run_command(self, command):
         return run_command(command, self.host, self.port)
 
-    def LoadFile(self):
+    def LoadRemoteFile(self):
         self.run_command('TGRUTint::instance()->OpenFileDialog()')
+
+    def LoadLocalFile(self):
+        filename = askopenfilename(filetypes=(("ROOT File", "*.root"),))
+        if not filename:
+            return
+
+        tfile = ROOT.TFile(filename)
+        self.files.append(tfile)
+
+        tree_id = self.hists.insert('','end',filename, text=filename)
+        for key in tfile.GetListOfKeys():
+            obj = key.ReadObj()
+            hist_name = obj.GetName()
+            if obj.Class().InheritsFrom('TH2'):
+                icon = self.icons['2d hist']
+            elif obj.Class().InheritsFrom('TH1'):
+                icon = self.icons['1d hist']
+            else:
+                icon = None
+
+            self.hists.insert(tree_id,'end',hist_name, text=hist_name,
+                              image=icon)
+
+
+    def Interpreter(self):
+        try:
+            import ptpython.repl; ptpython.repl.embed(globals(), locals())
+        except ImportError:
+            pass
+        else:
+            return
+
+        try:
+            import IPython; IPython.embed()
+        except ImportError:
+            pass
+        else:
+            return
+
+        import code; code.interact(local=locals())
 
     def _LoadTree(self):
         res = self.run_command('if(online_events) {'
@@ -131,21 +206,23 @@ class MainWindow(object):
     def ParameterSelection(self, event):
         print 'You clicked "{}"'.format(event.widget.selection()[0])
 
-    def _setup_GUI(self):
-        self.window.geometry('350x700')
-        self.window.wm_title("hist-o-matic")
-        self._LoadMenuBar()
-        self.window.config(menu=self.menubar)
-
-        button = tk.Button(self.window,
-                           text='Load File',fg="black",bg="goldenrod",
-                           command=self.LoadFile)
-        button.pack(fill=tk.X,expand=False)
-
-        self.tree = ttk.Treeview(self.window)
-        self.tree.pack(fill=tk.BOTH,expand=True)
-        self._LoadTree()
-        self.tree.bind("<Double-1>", self.ParameterSelection)
-
     def Run(self):
         self.window.mainloop()
+
+    def load_library(self, library_name, grut_lib = True):
+        if grut_lib:
+            library_name = os.path.join(os.path.dirname(__file__),
+                                        '..','libraries',library_name)
+        ROOT.gSystem.Load(library_name)
+
+    def hello(self):
+        print "hello!"
+
+    def open_canvas(self):
+        self.canvases.append(ROOT.GCanvas())
+
+    def set_refresh(self):
+        print("refresh = " + str(self.refreshrate.get()))
+
+    def set_plotlocation(self):
+        print("plot location = " + str(self.plotlocation.get()))
