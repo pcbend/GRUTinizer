d6ac57f58f401d6f0dea80b80800bc189a90b1c5
Eric Lunderberg <lunderberg@nscl.msu.edu>
2015-10-05 11:52:56 -0400

    Cleaned up argument parsing in buttons.py

--- buttons.py
+++ buttons.py
@@ -1,34 +1,19 @@
 #!/usr/bin/env python2
 
-from pygui.mainwindow import MainWindow
+import optparse
 import sys
 
-#if len(sys.argv)>1:
-#    port = int(sys.argv[1])
-#else:
-#port = 9090
+from pygui.mainwindow import MainWindow
 
+parser = optparse.OptionParser()
+parser.add_option('-p', dest='port', default=9090, type='int')
+(options, args) = parser.parse_args()
 
-suffix = ".root"
-prefix = "-p"
-files  = []
-port   = 9090
-for x in range(1,len(sys.argv)):
-    if sys.argv[x].endswith(suffix):
-        files.append(sys.argv[x])
-    elif sys.argv[x].startswith(prefix):
-        if(len(sys.argv[x])>2):
-            port = int(sys.argv[x][2:])
-        else:
-            x+=1
-            port = int(sys.argv[x])
-        print "port = " + str(port)
+port = options.port
+files = [f for f in args if f.endswith('.root')]
 
-#print "files len = " + str(len(files))
-#print "files = ", files
 window = MainWindow('localhost',port)
 for infile in files:
     window.LoadRootFile(infile)
-
 
 window.Run()
