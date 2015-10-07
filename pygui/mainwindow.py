ca983c0c03220add0ff61eb2cbafffa8d4e9db70
Eric Lunderberg <lunderberg@nscl.msu.edu>
2015-10-07 12:24:59 -0400

    Cleaning up of the mainwindow.py
    Separated out handling of the histogram tab and the tree tab into their own files.

--- pygui/mainwindow.py
+++ pygui/mainwindow.py
@@ -6,20 +6,17 @@ from tkFileDialog import askopenfilename
 import ttk
 
 import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
 import math
 
-from pygui.run_command import run_command
-from tree_structure import Node
-
+from .run_command import run_command
+from .tree_tab import TreeTab
+from .hist_tab import HistTab
 
 #Fix ROOT TCanvases, which don't redraw when they should
 def fix_tcanvases():
     for canvas in ROOT.gROOT.GetListOfCanvases():
         canvas.Update()
-
-def unpack_tdirectory(tdir):
-    for key in tdir.GetListOfKeys():
-        yield key.ReadObj()
 
 class MainWindow(object):
 
@@ -33,28 +30,6 @@ class MainWindow(object):
 
         self.window = tk.Tk()
         self._load_icons()
-
-        self.refreshrate  = tk.IntVar(value='-1')
-        self.plotlocation = tk.StringVar(value='NewCanvas')
-
-        self.predefinedzones = tk.StringVar(value='1x1')
-        self.zone_rows = 1
-        self.zone_cols = 1
-
-        self.optstat_name      = tk.BooleanVar(value=True)
-        self.optstat_entries   = tk.BooleanVar(value=True)
-        self.optstat_mean      = tk.BooleanVar(value=True)
-        self.optstat_mean_err  = tk.BooleanVar(value=False)
-        self.optstat_rms       = tk.BooleanVar(value=False)
-        self.optstat_rms_err   = tk.BooleanVar(value=False)
-        self.optstat_underflow = tk.BooleanVar(value=False)
-        self.optstat_overflow  = tk.BooleanVar(value=False)
-        self.optstat_integral  = tk.BooleanVar(value=True)
-        self.optstat_skew      = tk.BooleanVar(value=False)
-        self.optstat_skew_err  = tk.BooleanVar(value=False)
-        self.optstat_kurt      = tk.BooleanVar(value=False)
-        self.optstat_kurt_err  = tk.BooleanVar(value=False)
-
         self._load_default_style()
 
         self.canvases = []
@@ -93,18 +68,17 @@ class MainWindow(object):
     def _setup_GUI(self):
         self.window.geometry('350x700')
         self.window.wm_title("hist-o-matic")
-        self._LoadMenuBar()
-        self.window.config(menu=self.menubar)
+        self.window.config(menu=self._MakeMenuBar())
 
         frame = tk.Frame(self.window)
         button = tk.Button(frame,
                            text='Load Data File',fg="black",bg="goldenrod",
-                           command=self.LoadRemoteFile)
+                           command=self.LoadDataFile)
         button.pack(side=tk.LEFT)
 
         button = tk.Button(frame,
                            text='Load Root File',fg="black",bg="goldenrod",
-                           command=self.LoadLocalFile)
+                           command=self.LoadRootFile)
         button.pack(side=tk.LEFT)
 
         button = tk.Button(frame,
@@ -122,12 +96,11 @@ class MainWindow(object):
         notebook = ttk.Notebook(self.window)
 
         tree_page = ttk.Frame(notebook)
-        self._MakeHistogramControls(tree_page)
-        self._MakeTreeView(tree_page)
+        self.tree_tab = TreeTab(self, tree_page)
         notebook.add(tree_page, text='Tree Viewer')
 
         hist_page = ttk.Frame(notebook)
-        self._MakeHistView(hist_page)
+        self.hist_tab = HistTab(self, hist_page)
         notebook.add(hist_page, text='Histograms')
 
         notebook.pack(fill=tk.BOTH,expand=True)
@@ -162,18 +135,20 @@ class MainWindow(object):
                 stat += 'K'
         ROOT.gStyle.SetOptStat(stat)
 
-    def _LoadMenuBar(self):
-        self.menubar  = tk.Menu(self.window)
+    def _MakeMenuBar(self):
+        menubar  = tk.Menu(self.window)
 
-        self._MakeFileMenu()
-        self._MakeRefreshMenu()
-        self._MakeZoneMenu()
-        self._MakeOptStatMenu()
-        self._MakePlotMenu()
-        self._MakeHelpMenu()
+        self._MakeFileMenu(menubar)
+        self._MakeRefreshMenu(menubar)
+        self._MakeZoneMenu(menubar)
+        self._MakeOptStatMenu(menubar)
+        self._MakePlotMenu(menubar)
+        self._MakeHelpMenu(menubar)
 
-    def _MakeFileMenu(self):
-        filemenu = tk.Menu(self.menubar,tearoff=0)
+        return menubar
+
+    def _MakeFileMenu(self, menubar):
+        filemenu = tk.Menu(menubar,tearoff=0)
         filemenu.add_command(label="New Canvas",command=self.open_canvas)
         filemenu.add_separator()
         filemenu.add_command(label="Close All Canvases",command=self.close_all_canvases)
@@ -182,10 +157,12 @@ class MainWindow(object):
         filemenu.add_command(label="Save",command=self.hello)
         filemenu.add_separator()
         filemenu.add_command(label="Exit",command=self.window.quit)
-        self.menubar.add_cascade(label="File",menu=filemenu)
+        menubar.add_cascade(label="File",menu=filemenu)
 
-    def _MakeRefreshMenu(self):
-        refreshmenu = tk.Menu(self.menubar,tearoff=0)
+    def _MakeRefreshMenu(self,menubar):
+        self.refreshrate  = tk.IntVar(value='-1')
+
+        refreshmenu = tk.Menu(menubar,tearoff=0)
         refreshmenu.add_checkbutton(label="Off",onvalue=-1,
                                     variable=self.refreshrate)
         refreshmenu.add_command(label="Now!",command=self.RefreshHistograms)
@@ -200,7 +177,7 @@ class MainWindow(object):
                                     variable=self.refreshrate)
         refreshmenu.add_checkbutton(label="30 seconds",onvalue=30,
                                     variable=self.refreshrate)
-        self.menubar.add_cascade(label="Refresh",menu=refreshmenu)
+        menubar.add_cascade(label="Refresh",menu=refreshmenu)
         self.window.after_idle(self._PeriodicRefresh)
 
     def _PeriodicRefresh(self):
@@ -212,8 +189,12 @@ class MainWindow(object):
         self.window.after(self.refreshrate.get()*1000, self._PeriodicRefresh)
 
 
-    def _MakeZoneMenu(self):
-        zonesmenu = tk.Menu(self.menubar,tearoff=0)
+    def _MakeZoneMenu(self, menubar):
+        self.predefinedzones = tk.StringVar(value='1x1')
+        self.zone_rows = 1
+        self.zone_cols = 1
+
+        zonesmenu = tk.Menu(menubar,tearoff=0)
         zonesmenu.add_checkbutton(label="1 x 1",onvalue='1x1',
                                   variable=self.predefinedzones,command=self.set_zones)
         zonesmenu.add_checkbutton(label="1 x 2",onvalue='1x2',
@@ -232,7 +213,7 @@ class MainWindow(object):
                                   variable=self.predefinedzones,command=self.set_zones)
         zonesmenu.add_checkbutton(label="13 x 13",onvalue='13x13',
                                   variable=self.predefinedzones,command=self.set_zones)
-        self.menubar.add_cascade(label="Zones",menu=zonesmenu)
+        menubar.add_cascade(label="Zones",menu=zonesmenu)
 
     def set_zones(self,zones=""):
         if not zones:
@@ -242,10 +223,23 @@ class MainWindow(object):
         zones = zones.split("x")
         self.zone_cols = int(zones[0])
         self.zone_rows = int(zones[1])
-        print("zones set to " + str(self.zone_cols) + " x " + str(self.zone_rows))
 
-    def _MakeOptStatMenu(self):
-        optstatmenu = tk.Menu(self.menubar,tearoff=0)
+    def _MakeOptStatMenu(self, menubar):
+        self.optstat_name      = tk.BooleanVar(value=True)
+        self.optstat_entries   = tk.BooleanVar(value=True)
+        self.optstat_mean      = tk.BooleanVar(value=True)
+        self.optstat_mean_err  = tk.BooleanVar(value=False)
+        self.optstat_rms       = tk.BooleanVar(value=False)
+        self.optstat_rms_err   = tk.BooleanVar(value=False)
+        self.optstat_underflow = tk.BooleanVar(value=False)
+        self.optstat_overflow  = tk.BooleanVar(value=False)
+        self.optstat_integral  = tk.BooleanVar(value=True)
+        self.optstat_skew      = tk.BooleanVar(value=False)
+        self.optstat_skew_err  = tk.BooleanVar(value=False)
+        self.optstat_kurt      = tk.BooleanVar(value=False)
+        self.optstat_kurt_err  = tk.BooleanVar(value=False)
+
+        optstatmenu = tk.Menu(menubar,tearoff=0)
         optstatmenu.add_checkbutton(label="Name",onvalue=1,variable=self.optstat_name)
         optstatmenu.add_checkbutton(label="Entries",onvalue=1,variable=self.optstat_entries)
         optstatmenu.add_checkbutton(label="Mean",onvalue=1,variable=self.optstat_mean)
@@ -259,10 +253,12 @@ class MainWindow(object):
         optstatmenu.add_checkbutton(label="Skew Error",onvalue=1,variable=self.optstat_skew_err)
         optstatmenu.add_checkbutton(label="Kurtosis",onvalue=1,variable=self.optstat_kurt)
         optstatmenu.add_checkbutton(label="Kurtosis Error",onvalue=1,variable=self.optstat_kurt_err)
-        self.menubar.add_cascade(label="OptStats",menu=optstatmenu)
+        menubar.add_cascade(label="OptStats",menu=optstatmenu)
 
-    def _MakePlotMenu(self):
-        plotmenu = tk.Menu(self.menubar,tearoff=0)
+    def _MakePlotMenu(self, menubar):
+        self.plotlocation = tk.StringVar(value='NewCanvas')
+
+        plotmenu = tk.Menu(menubar,tearoff=0)
         plotmenu.add_checkbutton(label="New Canvas",onvalue='NewCanvas',
                                  variable=self.plotlocation)
         plotmenu.add_checkbutton(label="Next Pad",onvalue='NextPad',
@@ -271,134 +267,11 @@ class MainWindow(object):
                                  variable=self.plotlocation)
         plotmenu.add_checkbutton(label="Current Pad (overlay)",onvalue='Overlay',
                                  variable=self.plotlocation)
-        self.menubar.add_cascade(label="Plot",menu=plotmenu)
+        menubar.add_cascade(label="Plot",menu=plotmenu)
 
-    def _MakeHelpMenu(self):
-        helpmenu = tk.Menu(self.menubar,tearoff=0)
-        self.menubar.add_cascade(label="Send Help",menu=helpmenu)
-
-    def _MakeTreeView(self,parent):
-        self.tree = ttk.Treeview(parent)
-        self.tree.pack(fill=tk.BOTH,expand=True)
-        self._LoadTree()
-        self.tree.bind("<Double-1>", self.ParameterSelection)
-
-    def _MakeHistogramControls(self,parent):
-        self.next_param = 'x'
-
-        frame = tk.Frame(parent)
-
-        frame.columnconfigure(1,weight=1)
-
-        tk.Label(frame,text='Parameter').grid(row=0,column=1)
-        tk.Label(frame,text='Bins').grid(row=0,column=2)
-        tk.Label(frame,text='Low').grid(row=0,column=3)
-        tk.Label(frame,text='High').grid(row=0,column=4)
-
-        enable_x_label = tk.Label(frame,text='X',
-                                   anchor=tk.E)
-        enable_x_label.grid(row=1,column=0)
-
-        self.x_draw_varexp = tk.StringVar()
-        x_varexp = tk.Entry(frame,textvariable=self.x_draw_varexp)
-        x_varexp.grid(row=1,column=1,sticky='ew')
-
-        self.x_draw_bins = tk.StringVar(value='4096')
-        x_bins = tk.Entry(frame,textvariable=self.x_draw_bins,
-                          width=5,text='4096')
-        x_bins.grid(row=1,column=2)
-
-        self.x_draw_low = tk.StringVar(value='0')
-        x_low = tk.Entry(frame,textvariable=self.x_draw_low,
-                         width=5)
-        x_low.grid(row=1,column=3)
-
-        self.x_draw_high = tk.StringVar(value='4096')
-        x_high = tk.Entry(frame,textvariable=self.x_draw_high,
-                          width=5)
-        x_high.grid(row=1,column=4)
-
-
-
-        y_entry_boxes = []
-        def enable_y_parameters():
-            state = 'normal' if self.hist2d.get() else 'disabled'
-            for entry in y_entry_boxes:
-                entry.configure(state=state)
-
-        self.hist2d = tk.IntVar()
-        enable_y_box = tk.Checkbutton(frame, text='Y',
-                                      variable=self.hist2d,
-                                      command=enable_y_parameters)
-        enable_y_box.grid(row=2,column=0)
-
-
-        self.y_draw_varexp = tk.StringVar()
-        y_varexp = tk.Entry(frame,textvariable=self.y_draw_varexp)
-        y_varexp.grid(row=2,column=1,sticky='ew')
-        y_entry_boxes.append(y_varexp)
-
-        self.y_draw_bins = tk.StringVar(value='4096')
-        y_bins = tk.Entry(frame,textvariable=self.y_draw_bins,
-                          width=5)
-        y_bins.grid(row=2,column=2)
-        y_entry_boxes.append(y_bins)
-
-        self.y_draw_low = tk.StringVar(value='0')
-        y_low = tk.Entry(frame,textvariable=self.y_draw_low,
-                         width=5)
-        y_low.grid(row=2,column=3)
-        y_entry_boxes.append(y_low)
-
-        self.y_draw_high = tk.StringVar(value='4096')
-        y_high = tk.Entry(frame,textvariable=self.y_draw_high,
-                          width=5)
-        y_high.grid(row=2,column=4)
-        y_entry_boxes.append(y_high)
-
-        enable_y_parameters()
-
-        frame.pack(fill=tk.X,expand=False)
-
-        # Button frame
-        frame = tk.Frame(parent)
-        self.hist_name = tk.StringVar(value='hist')
-        tk.Label(frame,text='Name:').pack(side=tk.LEFT)
-        tk.Entry(frame,textvariable=self.hist_name).pack(side=tk.LEFT)
-        tk.Button(frame,text='Add Histogram',
-                  command=self.AddHistogram).pack(side=tk.LEFT)
-        frame.pack(fill=tk.X,expand=False)
-
-    def AddHistogram(self):
-        commands = {1:
-                    'if(online_events) {{'
-                    '    online_events->AddHistogram("{name}",'
-                    '                                {Xbins},{Xlow},{Xhigh},"{Xexp}");'
-                    '}}',
-                    2:
-                    'if(online_events) {{'
-                    '    online_events->AddHistogram("{name}",'
-                    '                                {Xbins},{Xlow},{Xhigh},"{Xexp}",'
-                    '                                {Ybins},{Ylow},{Yhigh},"{Yexp}");'
-                    '}}',
-        }
-
-        dimension = 2 if self.hist2d.get() else 1
-        command = commands[dimension]
-
-        args = {'name'  : self.hist_name.get(),
-                'Xbins' : self.x_draw_bins.get(),
-                'Xlow'  : self.x_draw_low.get(),
-                'Xhigh' : self.x_draw_high.get(),
-                'Xexp'  : self.x_draw_varexp.get(),
-                'Ybins' : self.y_draw_bins.get(),
-                'Ylow'  : self.y_draw_low.get(),
-                'Yhigh' : self.y_draw_high.get(),
-                'Yexp'  : self.y_draw_varexp.get(),
-            }
-        command = command.format(**args)
-        self.run_command(command)
-        self.RefreshHistograms()
+    def _MakeHelpMenu(self,menubar):
+        helpmenu = tk.Menu(menubar,tearoff=0)
+        menubar.add_cascade(label="Send Help",menu=helpmenu)
 
     def RefreshHistograms(self):
         online_hists = self.run_command('if(online_events) {'
@@ -406,27 +279,7 @@ class MainWindow(object):
                                         '}')
         if online_hists is not None:
             online_hists.SetName('online_hists')
-            self._insert_to_hist_tree(online_hists)
-
-    def _MakeHistView(self,parent):
-        self.hists = ttk.Treeview(parent)
-        self.hists.pack(fill=tk.BOTH,expand=True)
-        # Map from treeview name to ROOT object
-        self.hist_lookup = {}
-        self.hists.bind("<Double-1>", self.OnHistClick)
-
-    def OnHistClick(self,event):
-        objects = [self.hist_lookup[i] for i in event.widget.selection()]
-        histograms = [h for h in objects if h.InheritsFrom('TH1')]
-
-        color = 1;
-        for obj in histograms:
-            self._draw_single(obj,color,len(histograms))
-            if self.plotlocation.get()=='Overlay':
-                color+=1
-                if color == 5:
-                    color+=1
-
+            self.hist_tab.Insert(online_hists)
 
     def _draw_single(self,hist,color=1,nselected=1):
         canvas_exists = bool(filter(None,self.canvases))
@@ -452,14 +305,13 @@ class MainWindow(object):
         hist.Draw(' '.join(opt))
         fix_tcanvases()
 
-
     def run_command(self, command):
         return run_command(command, self.host, self.port)
 
-    def LoadRemoteFile(self):
+    def LoadDataFile(self):
         self.run_command('TGRUTint::instance()->OpenFileDialog()')
 
-    def LoadLocalFile(self):
+    def LoadRootFile(self):
         filename = askopenfilename(filetypes=(("ROOT File", "*.root"),))
         if not filename:
             return
@@ -469,43 +321,7 @@ class MainWindow(object):
         filename = os.path.abspath(filename)
         tfile = ROOT.TFile(filename)
         self.files[filename] = tfile
-        self._insert_to_hist_tree(tfile)
-
-    def _insert_to_hist_tree(self,obj,parent=''):
-        if obj.Class().InheritsFrom('TH2'):
-            icon = self.icons['h2_t']
-        elif obj.Class().InheritsFrom('TH1'):
-            icon = self.icons['h1_t']
-        elif obj.Class().InheritsFrom('TFile'):
-            icon = self.icons['tfile']
-        elif obj.Class().InheritsFrom('TDirectory'):
-            icon = self.icons['folder_t']
-        else:
-            icon = ''
-
-        name = parent + '/' + obj.GetName()
-
-        if (name in self.hists.get_children(parent) and
-            obj.Class().InheritsFrom('TH1')):
-            orig = self.hist_lookup[name]
-            obj.Copy(orig) # Copy the new object into the original
-            return
-        elif name in self.hists.get_children(parent):
-            self.hist_lookup[name] = obj
-        else:
-            self.hist_lookup[name] = obj
-            self.hists.insert(parent,'end', name, text=obj.GetName(),image=icon)
-
-        if obj.Class().InheritsFrom('TList'):
-            iterable = obj
-        elif hasattr(obj,'GetListOfKeys'):
-            iterable = unpack_tdirectory(obj)
-        else:
-            iterable = None
-
-        if iterable is not None:
-            for obj in iterable:
-                self._insert_to_hist_tree(obj, name)
+        self.hist_tab.Insert(tfile)
 
     def Interpreter(self):
         try:
@@ -524,34 +340,6 @@ class MainWindow(object):
 
         import code; code.interact(local=locals())
 
-    def _LoadTree(self):
-        res = self.run_command('if(online_events) {'
-                               '    gResponse = online_events->GetObjectStringLeaves();'
-                               '}')
-        if res is None:
-            return
-
-        res = filter(None,str(res).split('\n'))
-        tree = Node('')
-        for item in res:
-            tree.AddChild(item)
-        tree.ttk_treeview(self.tree)
-
-    def ParameterSelection(self, event):
-        param = event.widget.selection()[0]
-        if event.widget.get_children(param):
-            return
-
-        if not self.hist2d.get():
-            self.next_param = 'x'
-
-        if self.next_param == 'x':
-            self.x_draw_varexp.set(param)
-            self.next_param = 'y'
-        else:
-            self.y_draw_varexp.set(param)
-            self.next_param = 'x'
-
     def Run(self):
         self.window.mainloop()
 
@@ -568,7 +356,6 @@ class MainWindow(object):
         canvases = ROOT.gROOT.GetListOfCanvases()
         for canvas in canvases:
             canvas.Close()
-
 
     def open_canvas(self,title="",columns=-1,rows=-1,topx=0,topy=0,width=0,height=0):
         if not title:
@@ -594,6 +381,3 @@ class MainWindow(object):
         canvas.Update()
 
         self.canvases.append(canvas)
-
-    def set_refresh(self):
-        print("refresh = " + str(self.refreshrate.get()))
