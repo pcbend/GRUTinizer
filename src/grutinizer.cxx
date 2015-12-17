9614b467fd951bc9426b9081ebb1d7474e553b27
Eric Lunderberg <lunderberg@nscl.msu.edu>
2015-12-17 16:02:47 -0500

    If GRUTSYS is not defined, defines it to be one directory above the executable's directory.

--- src/grutinizer.cxx
+++ src/grutinizer.cxx
@@ -14,6 +14,7 @@
 #include "TThread.h"
 
 #include "TGRUTint.h"
+#include "ProgramPath.h"
 //#include "TROOT_Shim.h"
 //#include "LoadGRUTEnv.h"
 
@@ -54,7 +55,7 @@ void SetGRUTPluginHandlers();
 void LoadGRUTEnv() {
   // Set the GRUTSYS variable based on the executable path.
   // If GRUTSYS has already been defined, don't overwrite.
-  //setenv("GRUTSYS", (program_path()+"/..").c_str(), 0);
+  setenv("GRUTSYS", (program_path()+"/..").c_str(), 0);
   std::string grut_path = Form("%s/.grutrc",getenv("GRUTSYS")); // + "/../.grutrc";
   gEnv->ReadFile(grut_path.c_str(),kEnvChange);
 }
