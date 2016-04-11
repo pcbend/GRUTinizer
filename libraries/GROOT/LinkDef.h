2a0f2d5b1d1314e46c9f077e48fbc61aadab590e
Peter Bender <pcbend@gmail.com>
2016-04-11 12:19:02 -0400

    added ggaus.

--- libraries/GROOT/LinkDef.h
+++ libraries/GROOT/LinkDef.h
@@ -1,4 +1,4 @@
-// GRootGuiFactory.h GRootFunctions.h GRootCommands.h GRootCanvas.h GRootBrowser.h GCanvas.h GH2Base.h  GH2I.h GH2D.h  GPeak.h GValue.h GH1D.h GrutNotifier.h GPopup.h
+// GRootGuiFactory.h GRootFunctions.h GRootCommands.h GRootCanvas.h GRootBrowser.h GCanvas.h GH2Base.h  GH2I.h GH2D.h  GPeak.h GGaus.h GValue.h GH1D.h GrutNotifier.h GPopup.h
 
 
 #ifdef __CINT__
@@ -58,6 +58,7 @@
 #pragma link C++ class GH2D+;
 
 #pragma link C++ class GPeak+;
+#pragma link C++ class GGaus+;
 //#pragma link C++ class GEfficiency+;
 
 #pragma link C++ class GrutNotifier+;
