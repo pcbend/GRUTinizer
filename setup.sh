313a47c7976dc219f7952ee59f4928acfed37ca7
Chris Sullivan <sullivan@nscl.msu.edu>
2016-06-08 22:26:27 -0400

    Added setup script to create the necessary links to the GRAnalyzer git submodule

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GRANAFILES=("sca" "hb" "def" "alias" "datatest" "gr_wtdc.hst" "las_wtdc.hst" "hist.def")
+for linkey in "${GRANAFILES[@]}"; do
+	echo "ln -s GRAnalyzer/$linkey $linkey"
+	ln -s "GRAnalyzer/$linkey" "$linkey"
+done
