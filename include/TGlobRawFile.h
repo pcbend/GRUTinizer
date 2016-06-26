df11aef0b8f13a4017913fc717b23441892c9cab
Chris Sullivan <sullivan@nscl.msu.edu>
2016-06-27 00:48:18 +0900

    Added TGlobRawFile which looks in a directory for new files and attaches to them as they appears.

---
+++ include/TGlobRawFile.h
@@ -0,0 +1,52 @@
+#ifndef _TGLOBRAWFILE_H_
+#define _TGLOBRAWFILE_H_
+
+#ifndef __CINT__
+#include <chrono>
+#endif
+
+#include <string>
+
+#include "TRawSource.h"
+#include "TMultiRawFile.h"
+
+class TGlobRawFile : public TRawEventSource {
+public:
+  TGlobRawFile(std::string pattern);
+
+  virtual std::string SourceDescription(bool long_description=false) const {
+    return fWrapped.SourceDescription(long_description);
+  }
+  virtual std::string Status(bool long_description = false) const {
+    return fWrapped.Status(long_description);
+  }
+
+  virtual int GetLastErrno() const { return fWrapped.GetLastErrno(); }
+  virtual std::string GetLastError() const { return fWrapped.GetLastError(); }
+
+  virtual void Reset() {
+    TRawEventSource::Reset();
+    fWrapped.Reset();
+  }
+
+
+private:
+  virtual int GetEvent(TRawEvent& outevent) {
+    CheckForFiles();
+    return fWrapped.Read(outevent);
+  }
+
+  void CheckForFiles();
+
+  std::string fPattern;
+  std::set<std::string> fFilesAdded;
+#ifndef __CINT__
+  std::chrono::system_clock::time_point fPreviousCheck;
+#endif
+
+  TMultiRawFile fWrapped;
+
+  ClassDef(TGlobRawFile, 0);
+};
+
+#endif /* _TGLOBRAWFILE_H_ */
