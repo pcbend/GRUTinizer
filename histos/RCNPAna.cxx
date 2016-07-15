5d1e8346149de5b37a64c60e5531fd379a50b8df
Chris Sullivan <sullivan@nscl.msu.edu>
2016-07-15 16:34:18 -0400

    Added fix to tcut_tab.py so that Save Cut works when the det_type is not registered. Also added many focal plane corrections.

--- histos/RCNPAna.cxx
+++ histos/RCNPAna.cxx
@@ -12,6 +12,7 @@
 #include <TMath.h>
 #include <TRandom.h>
 #include <TObject.h>
+#include <TFile.h>
 
 #include "TCagra.h"
 #include "TGrandRaiden.h"
@@ -30,12 +31,29 @@ using namespace std;
 string name;
 stringstream stream;
 
+static TFile fcuts("bg_cuts.root");
+static auto cut0 = dynamic_cast<TCutG*>(fcuts.Get("_cut0"));
+static auto cut1 = dynamic_cast<TCutG*>(fcuts.Get("_cut1"));
+static auto cut2 = dynamic_cast<TCutG*>(fcuts.Get("_cut2"));
+static auto cut3 = dynamic_cast<TCutG*>(fcuts.Get("_cut3"));
+
 
 // extern "C" is needed to prevent name mangling.
 // The function signature must be exactly as shown here,
 //   or else bad things will happen.
 extern "C"
 void MakeHistograms(TRuntimeObjects& obj) {
+
+
+  ///////////////////////////
+  // staic initializations //
+  ///////////////////////////
+
+
+  //reinterpret_cast<TCutG*>(fcuts->Get("_cut0"))->IsInside
+
+
+  ///////////////////////////
 
 
 
@@ -66,26 +84,33 @@ void MakeHistograms(TRuntimeObjects& obj) {
       for (auto& caghit : *cagra) {
 
         auto cagratime = caghit.Timestamp();
-	auto tdiff = cagratime-grtime;
+        auto tdiff = cagratime-grtime;
+        auto boardid = caghit.GetBoardID();
+        auto channel = caghit.GetChannel();
+
         obj.FillHistogram("COIN","Diff",1000,-1000,1000,cagratime-grtime);
         stream.str("");
-        stream << "TimeDiff_" << caghit.GetBoardID() << "_" <<caghit.GetChannel();
+        stream << "TimeDiff_" << boardid << "_" <<channel;
         obj.FillHistogram("COIN",stream.str().c_str(),1200,-300,800,tdiff);
 	if ( (tdiff > 235) && (tdiff < 245) ){
 
 	  stream.str("");
-          stream << "Ge_"<< caghit.GetBoardID() << "_" << caghit.GetChannel();
+          stream << "Ge_"<< boardid << "_" <<channel;
           obj.FillHistogram("COIN_Raw",stream.str(),10000,0,10000,caghit.Charge());
 	  stream.str("");
-          stream << "Ge_" << caghit.GetBoardID() << "_" << caghit.GetChannel();
+          stream << "Ge_" << boardid << "_" <<channel;
           obj.FillHistogram("COIN_Calibrated",stream.str(),10000,0,10000,caghit.GetEnergy());
 
 	  stream.str("");
-          stream << "Ex_Ge_" << caghit.GetBoardID() << "_" << caghit.GetChannel();
+          stream << "Ex_Ge_" << boardid << "_" <<channel;
           obj.FillHistogram("COIN_Calibrated",stream.str(),1000,0,20,Ex,2500,0,10,caghit.GetEnergy()*0.0010552+0.0636885);
 
+
+          stream << "Ge_PZ_AsymBL_" << boardid << "_" << channel;
+          obj.FillHistogram("COIN_Calibrated", stream.str().c_str(),10000,0,10000,caghit.GetCorrectedEnergy(caghit.GetBaseSample()));
+
           stream.str("");
-          stream << "Ex_CAGRACorrected_" << caghit.GetBoardID() << "_" << caghit.GetChannel();
+          stream << "Ex_CAGRACorrected_" << boardid << "_" <<channel;
           obj.FillHistogram("COIN_Calibrated", stream.str().c_str(),1000,0,20,Ex,2500,0,10000,caghit.GetCorrectedEnergy(caghit.GetBaseSample()));
 	}
 
@@ -106,7 +131,10 @@ void MakeHistograms(TRuntimeObjects& obj) {
   }
 
 
+
   if (gr) {
+
+    std::function<void(std::string)> fp_corrections;
 
     for (auto& hit : *gr) {
 
@@ -139,9 +167,87 @@ void MakeHistograms(TRuntimeObjects& obj) {
         obj.FillHistogram("GR","MeanPlastE1", 2000,0,2000, hit.GetMeanPlastE1());
         obj.FillHistogram("GR","MeanPlastE2", 2000,0,2000, hit.GetMeanPlastE2());
         if (rf != BAD_NUM) {
-          obj.FillHistogram("GR","dE1_RF",1000,0,0,rf,2000,0,2000, hit.GetMeanPlastE1());
-          obj.FillHistogram("GR","dE2_RF",1000,0,0,rf,2000,0,2000, hit.GetMeanPlastE2());
-          obj.FillHistogram("GR","dE2_dE1",2000,0,2000, hit.GetMeanPlastE2(),2000,0,2000, hit.GetMeanPlastE1());
+          obj.FillHistogram("GR","dE1[RF]",1000,0,0,rf,2000,0,2000, hit.GetMeanPlastE1());
+          obj.FillHistogram("GR","dE2[RF]",1000,0,0,rf,2000,0,2000, hit.GetMeanPlastE2());
+          obj.FillHistogram("GR","dE1[dE2]",2000,0,2000, hit.GetMeanPlastE2(),2000,0,2000, hit.GetMeanPlastE1());
+
+          fp_corrections = [&](string dir) {
+            obj.FillHistogram(dir.c_str(),"RF[X]",1200,-600,600, rcnp.GR_X(0),500,700,1200,rf);
+            obj.FillHistogram(dir.c_str(),"RF[TH]",1000,-1,1, rcnp.GR_TH(0),500,700,1200,rf);
+            obj.FillHistogram(dir.c_str(),"RF[TH]cor",1000,-1,1, rcnp.GR_TH(0),500,700,1200,rf-(-1870.45+355.461)*rcnp.GR_TH(0));
+            auto rf_cor = rf - (-1870.45+355.461)*rcnp.GR_TH(0);
+            obj.FillHistogram(dir.c_str(),"RF[X]_THcor",1200,-600,600, rcnp.GR_X(0),500,700,1200,rf_cor);
+            obj.FillHistogram(dir.c_str(),"RF[X]_THcor_Xcor",1200,-600,600, rcnp.GR_X(0),500,700,1200,rf_cor-0.177134*rcnp.GR_X(0));
+            rf_cor -= 0.177134*rcnp.GR_X(0);
+            obj.FillHistogram(dir.c_str(),"dE1[RF_cor]",1000,0,0,rf_cor,2000,0,2000, hit.GetMeanPlastE1());
+            obj.FillHistogram(dir.c_str(),"dE2[RF_cor]",1000,0,0,rf_cor,2000,0,2000, hit.GetMeanPlastE2());
+
+
+            obj.FillHistogram(dir.c_str(),"dE1[X]",1200,-600,600, rcnp.GR_X(0),2000,0,2000, hit.GetMeanPlastE1());
+            obj.FillHistogram(dir.c_str(),"dE1[TH]",1000,-1,1, rcnp.GR_TH(0),2000,0,2000, hit.GetMeanPlastE1());
+            auto dE1 = hit.GetMeanPlastE1() - (-368.343)*rcnp.GR_TH(0);
+            obj.FillHistogram(dir.c_str(),"dE1[TH]_cor",1000,-1,1, rcnp.GR_TH(0),2000,0,2000, dE1);
+            obj.FillHistogram(dir.c_str(),"dE1_THcor[dE2]",2000,0,2000, hit.GetMeanPlastE2(),2000,0,2000, dE1);
+            obj.FillHistogram(dir.c_str(),"RF_cor[dE1_THcor]",1000,0,0,rf_cor,2000,0,2000, dE1); //BOOM! :0)
+
+
+            if (cut0->IsInside(rcnp.GR_X(0),rcnp.GR_TH(0)) ||
+                cut1->IsInside(rcnp.GR_X(0),rcnp.GR_TH(0)) ||
+                cut2->IsInside(rcnp.GR_X(0),rcnp.GR_TH(0)) ||
+                cut3->IsInside(rcnp.GR_X(0),rcnp.GR_TH(0))
+              ) {
+              obj.FillHistogram(dir+"_BG","X_TH",1200,-600,600,rcnp.GR_X(0),1000,-1,1,rcnp.GR_TH(0));
+              obj.FillHistogram(dir+"_BG","dE1[RF]",1000,0,0,rf,2000,0,2000, hit.GetMeanPlastE1());
+              obj.FillHistogram(dir+"_BG","dE2[RF]",1000,0,0,rf,2000,0,2000, hit.GetMeanPlastE2());
+              obj.FillHistogram(dir+"_BG","dE1[RF_cor]",1000,0,0,rf_cor,2000,0,2000, hit.GetMeanPlastE1());
+              obj.FillHistogram(dir+"_BG","dE2[RF_cor]",1000,0,0,rf_cor,2000,0,2000, hit.GetMeanPlastE2());
+              obj.FillHistogram(dir+"_BG","dE1_THcor[RF_cor]",1000,0,0,rf_cor,2000,0,2000, dE1);
+              obj.FillHistogram(dir+"_BG","dE1[X]",1200,-600,600, rcnp.GR_X(0),2000,0,2000, hit.GetMeanPlastE1());
+              obj.FillHistogram(dir+"_BG","dE1[TH]",1000,-1,1, rcnp.GR_TH(0),2000,0,2000, hit.GetMeanPlastE1());
+              obj.FillHistogram(dir+"_BG","dE2[X]",1200,-600,600, rcnp.GR_X(0),2000,0,2000, hit.GetMeanPlastE2());
+              obj.FillHistogram(dir+"_BG","dE2[TH]",1000,-1,1, rcnp.GR_TH(0),2000,0,2000, hit.GetMeanPlastE2());
+
+            } else {
+              obj.FillHistogram(dir.c_str(),"RF_cor[dE1_THcor]_NOTbgcuts",1000,0,0,rf_cor,2000,0,2000, dE1);
+            }
+
+
+
+            dE1 -= ( -0.0007363*rcnp.GR_X(0)+0.00014677*rcnp.GR_X(0)*rcnp.GR_X(0)  );
+            obj.FillHistogram(dir.c_str(),"dE1_THcor[X]_cor",1200,-600,600, rcnp.GR_X(0),2000,0,2000, dE1);
+            obj.FillHistogram(dir.c_str(),"RF_cor[dE1_THcor_Xcor]",1000,0,0,rf_cor,2000,0,2000, dE1);
+
+
+            obj.FillHistogram(dir.c_str(),"dE2[X]",1200,-600,600, rcnp.GR_X(0),2000,0,2000, hit.GetMeanPlastE2());
+            obj.FillHistogram(dir.c_str(),"dE2[TH]",1000,-1,1, rcnp.GR_TH(0),2000,0,2000, hit.GetMeanPlastE2());
+            auto dE2 = hit.GetMeanPlastE2() - (980.2*rcnp.GR_TH(0) - 10478.3*rcnp.GR_TH(0)*rcnp.GR_TH(0));
+            obj.FillHistogram(dir.c_str(),"dE2[TH]_cor",1000,-1,1, rcnp.GR_TH(0),2000,0,2000, dE2);
+            obj.FillHistogram(dir.c_str(),"dE2_THcor_dE1_THcor",2000,0,2000,dE2,2000,0,2000, dE1);
+            obj.FillHistogram(dir.c_str(),"RF_cor[dE2_THcor]",1000,0,0,rf_cor,2000,0,2000, dE2);
+
+
+            // dE2 -= (0.023313*rcnp.GR_X(0)+
+            //   -0.00099883*TMath::Power(rcnp.GR_X(0),2)+
+            //   -2.42237e-06*TMath::Power(rcnp.GR_X(0),3)+
+            //   1.77828e-08*TMath::Power(rcnp.GR_X(0),4)+
+            //   6.2841e-12*TMath::Power(rcnp.GR_X(0),5)+
+            //   -1.02436e-13*TMath::Power(rcnp.GR_X(0),6)+
+            //   6.40518e-18*TMath::Power(rcnp.GR_X(0),7)+
+            //   1.58232e-19*TMath::Power(rcnp.GR_X(0),8)+
+            //   -9.92371e-24*TMath::Power(rcnp.GR_X(0),9));
+            // obj.FillHistogram(dir.c_str(),"dE2[X]_cor9",1200,-600,600, rcnp.GR_X(0),2000,0,2000, dE2);
+
+
+
+
+
+
+          };
+          if (rcnp.GR_RAYID(0) == 0) { // if track reconstruction successfull
+            fp_corrections("GR");
+          }
+
+
 
         }
       }
@@ -170,6 +276,9 @@ void MakeHistograms(TRuntimeObjects& obj) {
 
           if (qtc_leading[i]>=-5100 && qtc_leading[i] <=-4300) {
 
+
+
+
             obj.FillHistogram("GR_Prompt","RayID",64,-16,48, rcnp.GR_RAYID(0));
             if (rcnp.GR_RAYID(0) == 0) { // if track reconstruction successfull
               obj.FillHistogram("GR_Prompt","GR_X",1200,-600,600, rcnp.GR_X(0));
@@ -181,6 +290,13 @@ void MakeHistograms(TRuntimeObjects& obj) {
               obj.FillHistogram("GR_Prompt","GR_Theta_Phi",100,-1,1, rcnp.GR_TH(0),100,-1,1, rcnp.GR_PH(0)); // need to learn
               obj.FillHistogram("GR_Prompt","GR_X_Y",1200,-600,600, rcnp.GR_X(0),200,-100,100, rcnp.GR_Y(0));
               obj.FillHistogram("GR_Prompt","GR_X_cal",1000,0,20, Ex);
+
+
+              fp_corrections("GR_Prompt");
+
+
+
+
 
             }
 
@@ -292,7 +408,7 @@ void MakeHistograms(TRuntimeObjects& obj) {
       stream << "Ge_PZ_" << boardid << "_" << chan;
       obj.FillHistogram("CAGRA_Corrected", stream.str().c_str(),10000,0,10000,hit.GetCorrectedEnergy(0));
       stream.str("");
-      stream << "Ge_PZ_AsymBL_" << boardid << "Cal" << chan;
+      stream << "Ge_PZ_AsymBL_" << boardid << "_" << chan;
       obj.FillHistogram("CAGRA_Corrected", stream.str().c_str(),10000,0,10000,hit.GetCorrectedEnergy(hit.GetBaseSample()));
 
       Double_t prerise_base = hit.GetPreRise()/TANLEvent::GetShapingTime();
