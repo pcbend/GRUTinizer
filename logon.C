c99da787b6c6b4638016eb1d85605afbc7d6a42d
Chris Sullivan <sullivan@nscl.msu.edu>
2016-07-19 12:10:33 -0400

    Added delay to StoppableThread::StopAll after shutting down the RCNP thread, to avoid a deadlock owing to an incomplete push after the GRUTinizer threads have shutdown. Also changed the behavior of the RCNP stdout so that it only prints status to screen in offline mode (ie without gui).

--- logon.C
+++
@@ -1,7 +0,0 @@
-void logon() {
-  // set tick settings
-  gStyle->SetTitleOffset(1.2,"x");
-  gStyle->SetTitleOffset(1.2,"y");
-  gStyle->SetTickLength(-0.0125, "xyz");
-  gStyle->SetLabelOffset(0.015, "xyz");
-}
