21afb482a936c0488ebe5f28a9bf4d4c2a0938cd
Chris Sullivan <sullivan@nscl.msu.edu>
2016-07-19 10:14:38 -0400

    added logon script and changed rint logon script path in grutrc

---
+++ logon.C
@@ -0,0 +1,7 @@
+void logon() {
+  // set tick settings
+  gStyle->SetTitleOffset(1.2,"x");
+  gStyle->SetTitleOffset(1.2,"y");
+  gStyle->SetTickLength(-0.0125, "xyz");
+  gStyle->SetLabelOffset(0.015, "xyz");
+}
