7ef0dd7bf9ca5ce54ad7c781ad474123500c6d93
Peter Bender <pcbend@gmail.com>
2017-03-22 11:34:43 -0400

    more functions

--- libraries/GROOT/GGaus.cxx
+++ libraries/GROOT/GGaus.cxx
@@ -10,7 +10,7 @@
 #include "GRootFunctions.h"
 #include "GCanvas.h"
 
-ClassImp(GGaus);
+ClassImp(GGaus)
 
 GGaus::GGaus(Double_t xlow,Double_t xhigh,Option_t *opt)
   : TF1("gausbg","gaus(0)+pol1(3)",xlow,xhigh,TF1::EAddToList::kNo),
