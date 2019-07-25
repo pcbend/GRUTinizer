f99e69f595601999529861726c60ce65e6ca389f
gretina <gretina@gmail.com>
2019-07-24 22:37:19 -0400

    dimension update

--- pygui/mainwindow.py
+++ pygui/mainwindow.py
@@ -370,6 +370,8 @@ class MainWindow(object):
                                   variable=self.predefinedzones,command=self.set_zones)
         zonesmenu.add_checkbutton(label="1 x 3",onvalue='1x3',
                                   variable=self.predefinedzones,command=self.set_zones)
+        zonesmenu.add_checkbutton(label="1 x 4",onvalue='1x4',
+                                  variable=self.predefinedzones,command=self.set_zones)
         zonesmenu.add_checkbutton(label="2 x 1",onvalue='2x1',
                                   variable=self.predefinedzones,command=self.set_zones)
         zonesmenu.add_checkbutton(label="2 x 2",onvalue='2x2',
