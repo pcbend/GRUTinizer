a0e1c6247e0c9e7e564a66f49086bf7f4aed47eb
pcbend <pcbend@gmail.com>
2020-06-01 11:48:12 -0400

    removed parent get from child projection in canvas

--- libraries/GROOT/GCanvas.cxx
+++ libraries/GROOT/GCanvas.cxx
@@ -1066,21 +1066,21 @@ bool GCanvas::Process1DKeyboardPress(Event_t *event,UInt_t *keysym) {
       }
       break;
 
-    case kKey_P: {
-                   GH1D* ghist = NULL;
-                   for(auto hist : hists){
-                     if(hist->InheritsFrom(GH1D::Class())){
-                       ghist = (GH1D*)hist;
-                       break;
-                     }
-                   }
-
-                   if(ghist && ghist->GetHistParent()) {
-                     ghist->GetHistParent()->Draw();
-                     edited=true;
-                   }
-                 }
-                 break;
+//    case kKey_P: {
+//                 GH1D* ghist = NULL;
+//                 for(auto hist : hists){
+//                   if(hist->InheritsFrom(GH1D::Class())){
+//                     ghist = (GH1D*)hist;
+//                     break;
+//                   }
+//                 }
+//
+//                 if(ghist && ghist->GetHistParent()) {
+//                   ghist->GetHistParent()->Draw();
+//                   edited=true;
+//                 }
+//               }
+//               break;
     case kKey_q:{
                   TH1* ghist = hists.at(0);
                   if(GetNMarkers()>1) {
